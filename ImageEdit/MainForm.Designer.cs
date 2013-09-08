8b3e81963284500d1bf820feadfcd2f0a69afe32
Daniel Posada <daniel.posada+github@gmail.com>
2013-09-08 15:09:46 -0500

    Initial add of the C# source

---
+++ ImageEdit/MainForm.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace ImageEdit
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.x = new System.Windows.Forms.TextBox();
+            this.y = new System.Windows.Forms.TextBox();
+            this.width = new System.Windows.Forms.TextBox();
+            this.height = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.extensions = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(88, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Choose Dir and Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // x
+            // 
+            this.x.Location = new System.Drawing.Point(88, 12);
+            this.x.Name = "x";
+            this.x.Size = new System.Drawing.Size(124, 20);
+            this.x.TabIndex = 1;
+            // 
+            // y
+            // 
+            this.y.Location = new System.Drawing.Point(88, 38);
+            this.y.Name = "y";
+            this.y.Size = new System.Drawing.Size(124, 20);
+            this.y.TabIndex = 2;
+            // 
+            // width
+            // 
+            this.width.Location = new System.Drawing.Point(88, 64);
+            this.width.Name = "width";
+            this.width.Size = new System.Drawing.Size(124, 20);
+            this.width.TabIndex = 3;
+            // 
+            // height
+            // 
+            this.height.Location = new System.Drawing.Point(88, 90);
+            this.height.Name = "height";
+            this.height.Size = new System.Drawing.Size(124, 20);
+            this.height.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Y";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Width";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Height";
+            // 
+            // extensions
+            // 
+            this.extensions.Location = new System.Drawing.Point(88, 116);
+            this.extensions.Name = "extensions";
+            this.extensions.Size = new System.Drawing.Size(124, 20);
+            this.extensions.TabIndex = 9;
+            this.extensions.Text = "*.jpg;*.bmp";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "File Extensions";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(224, 176);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.extensions);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.height);
+            this.Controls.Add(this.width);
+            this.Controls.Add(this.y);
+            this.Controls.Add(this.x);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Image Edit v0.2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox x;
+        private System.Windows.Forms.TextBox y;
+        private System.Windows.Forms.TextBox width;
+        private System.Windows.Forms.TextBox height;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox extensions;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ ImageEdit/MainForm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ImageEdit
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var result = this.folderBrowserDialog1.ShowDialog(this);
+            if (result == DialogResult.OK)
+            {
+                var directory = this.folderBrowserDialog1.SelectedPath;
+                var dirInfo = new DirectoryInfo(directory);
+                CropImagesInDirectoryRecursive(dirInfo, dirInfo.Name);
+            }
+        }
+
+        private void CropImagesInDirectoryRecursive(DirectoryInfo dirInfo, string rootDirName)
+        {
+            var split = extensions.Text.Split(new[] {';'}, StringSplitOptions.RemoveEmptyEntries);
+            var trimmed = split.Select(e => e.Trim());
+            foreach (var fileInfo in trimmed.SelectMany(dirInfo.GetFiles))
+            {
+                CropImage(fileInfo, rootDirName);
+            }
+            foreach (var directoryInfo in dirInfo.GetDirectories())
+            {
+                CropImagesInDirectoryRecursive(directoryInfo, rootDirName);
+            }
+        }
+
+        private void CropImage(FileInfo fileInfo, string rootDirName)
+        {
+            var oldBitmap = new Bitmap(fileInfo.FullName);
+            var cropX = int.Parse(this.x.Text);
+            var cropY = int.Parse(this.y.Text);
+            var cropWidth = int.Parse(this.width.Text);
+            var cropHeight = int.Parse(this.height.Text);
+            var rectangle = new Rectangle(cropX, cropY, cropWidth, cropHeight);
+            var newBitmap = CropImage(oldBitmap, rectangle);
+            var newDir = fileInfo.Directory.FullName.Replace(rootDirName, rootDirName + "_New");
+            Directory.CreateDirectory(newDir);
+            newBitmap.Save(newDir+"\\" + fileInfo.Name);
+        }
+
+        public Bitmap CropImage(Bitmap source, Rectangle section)
+        {
+            // An empty bitmap which will hold the cropped image
+            var bmp = new Bitmap(section.Width, section.Height);
+
+            var g = Graphics.FromImage(bmp);
+
+            // Draw the given area (section) of the source image
+            // at location 0,0 on the empty bitmap (bmp)
+            g.DrawImage(source, 0, 0, section, GraphicsUnit.Pixel);
+
+            return bmp;
+        }
+    }
+}
